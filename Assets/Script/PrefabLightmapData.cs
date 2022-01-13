056f7bd93155589f3b3c6aa6df2b6fec34a12448
WangChong <654306663@qq.com>
2022-01-13 14:22:17 +0800

    解决发布后异常问题

--- Assets/Script/PrefabLightmapData.cs
+++ Assets/Script/PrefabLightmapData.cs
@@ -1,114 +1,110 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
-
 
 public class PrefabLightmapData : MonoBehaviour
 {
-    [System.Serializable]
-    public struct RendererInfo
-    {
-        public Renderer renderer;
-        public int lightmapIndex;
-        public Vector4 lightmapOffsetScale;
-    }
-    
-    public RendererInfo[] m_RendererInfo;
-    public Texture2D[] m_Lightmaps;
+	[System.Serializable]
+	struct RendererInfo
+	{
+		public Renderer 	renderer;
+		public int 			lightmapIndex;
+		public Vector4 		lightmapOffsetScale;
+	}
 
+	[SerializeField]
+	RendererInfo[]	m_RendererInfo;
+	[SerializeField]
+	Texture2D[] 	m_Lightmaps;
 
-    void Awake()
-    {
-        Init();
-    }
+	void Awake ()
+	{
+		if (m_RendererInfo == null || m_RendererInfo.Length == 0)
+			return;
 
-    void Init()
-    {
-        if (m_RendererInfo == null || m_RendererInfo.Length == 0)
-            return;
+		var lightmaps = LightmapSettings.lightmaps;
+		var combinedLightmaps = new LightmapData[lightmaps.Length + m_Lightmaps.Length];
 
-        var lightmaps = LightmapSettings.lightmaps;
-        int[] offsetsindexes = new int[m_Lightmaps.Length];
-        int counttotal = lightmaps.Length;
-        List<LightmapData> combinedLightmaps = new List<LightmapData>();
+		lightmaps.CopyTo(combinedLightmaps, 0);
+		for (int i = 0; i < m_Lightmaps.Length;i++)
+		{
+			combinedLightmaps[i+lightmaps.Length] = new LightmapData();
+			combinedLightmaps[i+lightmaps.Length].lightmapColor = m_Lightmaps[i];
+		}
 
-        for (int i = 0; i < m_Lightmaps.Length; i++)
-        {
-            bool exists = false;
-            for (int j = 0; j < lightmaps.Length; j++)
-            {
+		ApplyRendererInfo(m_RendererInfo, lightmaps.Length);
+		LightmapSettings.lightmapsMode = LightmapsMode.NonDirectional;
+		LightmapSettings.lightmaps = combinedLightmaps;
+	}
 
-                if (m_Lightmaps[i] == lightmaps[j].lightmapColor)
-                {
-                    exists = true;
-                    offsetsindexes[i] = j;
+	
+	static void ApplyRendererInfo (RendererInfo[] infos, int lightmapOffsetIndex)
+	{
+		for (int i=0;i<infos.Length;i++)
+		{
+			var info = infos[i];
+			info.renderer.lightmapIndex = info.lightmapIndex + lightmapOffsetIndex;
+			info.renderer.lightmapScaleOffset = info.lightmapOffsetScale;
+		}
+	}
 
-                }
+#if UNITY_EDITOR
+	[UnityEditor.MenuItem("Assets/Bake Prefab Lightmaps")]
+	static void GenerateLightmapInfo ()
+	{
+		if (UnityEditor.Lightmapping.giWorkflowMode != UnityEditor.Lightmapping.GIWorkflowMode.OnDemand)
+		{
+			Debug.LogError("ExtractLightmapData requires that you have baked you lightmaps and Auto mode is disabled.");
+			return;
+		}
+		UnityEditor.Lightmapping.Bake();
 
-            }
-            if (!exists)
-            {
-                offsetsindexes[i] = counttotal;
-                var newlightmapdata = new LightmapData();
-                newlightmapdata.lightmapColor = m_Lightmaps[i];
-                combinedLightmaps.Add(newlightmapdata);
+		PrefabLightmapData[] prefabs = FindObjectsOfType<PrefabLightmapData>();
 
-                counttotal += 1;
+		foreach (var instance in prefabs)
+		{
+			var gameObject = instance.gameObject;
+			var rendererInfos = new List<RendererInfo>();
+			var lightmaps = new List<Texture2D>();
+			
+			GenerateLightmapInfo(gameObject, rendererInfos, lightmaps);
+			
+			instance.m_RendererInfo = rendererInfos.ToArray();
+			instance.m_Lightmaps = lightmaps.ToArray();
 
+			var targetPrefab = UnityEditor.PrefabUtility.GetPrefabParent(gameObject) as GameObject;
+			if (targetPrefab != null)
+			{
+				//UnityEditor.Prefab
+				UnityEditor.PrefabUtility.ReplacePrefab(gameObject, targetPrefab);
+			}
+		}
+	}
 
-            }
+	static void GenerateLightmapInfo (GameObject root, List<RendererInfo> rendererInfos, List<Texture2D> lightmaps)
+	{
+		var renderers = root.GetComponentsInChildren<MeshRenderer>();
+		foreach (MeshRenderer renderer in renderers)
+		{
+			if (renderer.lightmapIndex != -1)
+			{
+				RendererInfo info = new RendererInfo();
+				info.renderer = renderer;
+				info.lightmapOffsetScale = renderer.lightmapScaleOffset;
 
-        }
+				Texture2D lightmap = LightmapSettings.lightmaps[renderer.lightmapIndex].lightmapColor;
 
-        var combinedLightmaps2 = new LightmapData[counttotal];
+				info.lightmapIndex = lightmaps.IndexOf(lightmap);
+				if (info.lightmapIndex == -1)
+				{
+					info.lightmapIndex = lightmaps.Count;
+					lightmaps.Add(lightmap);
+				}
 
-        lightmaps.CopyTo(combinedLightmaps2, 0);
-        combinedLightmaps.ToArray().CopyTo(combinedLightmaps2, lightmaps.Length);
-        LightmapSettings.lightmapsMode = LightmapsMode.NonDirectional;
-        ApplyRendererInfo(m_RendererInfo, offsetsindexes);
-        LightmapSettings.lightmaps = combinedLightmaps2;
-    }
+				rendererInfos.Add(info);
+			}
+		}
+	}
+#endif
 
-    void OnEnable()
-    {
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    // called second
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        Init();
-    }
-
-    // called when the game is terminated
-    void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-
-
-    static void ApplyRendererInfo(RendererInfo[] infos, int[] lightmapOffsetIndex)
-    {
-        for (int i = 0; i < infos.Length; i++)
-        {
-            var info = infos[i];
-
-            info.renderer.lightmapIndex = lightmapOffsetIndex[info.lightmapIndex];
-            info.renderer.lightmapScaleOffset = info.lightmapOffsetScale;
-
-            // You have to release shaders.
-            Material[] mat = info.renderer.sharedMaterials;
-            for (int j = 0; j < mat.Length; j++)
-            {
-                if (mat[j] != null && Shader.Find(mat[j].shader.name) != null)
-                {
-                    mat[j].shader = Shader.Find(mat[j].shader.name);
-                }
-            }
-
-        }
-    }
 }
